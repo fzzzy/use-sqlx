469edfc11b3e4f65e896bf37f36a60a1d6b4a752
Donovan Preston <donovanpreston@gmail.com>
2020-10-15 20:19:49 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use sqlx::prelude::*;
-use sqlx::{Cursor, mysql::MySqlPool};
+use sqlx::{Cursor, mysql::{MySqlPool, MySqlRow}};
 use async_std::task;
+use futures::TryStreamExt;
 
 #[derive(Debug)]
 enum MyError {
@@ -21,6 +22,19 @@ impl From<env::VarError> for MyError {
     }
 }
 
+#[derive(Debug)]
+struct TokenserverUser {
+    uid: i64,
+    service: i32,
+    email: String,
+    generation: i64,
+    client_state: String,
+    created_at: i64,
+    replaced_at: Option<i64>,
+    nodeid: i64,
+    keys_changed_at: Option<i64>,
+}
+
 async fn async_main() -> Result<(), MyError> {
     // Create a connection pool
     let pool = MySqlPool::builder()
@@ -28,18 +42,26 @@ async fn async_main() -> Result<(), MyError> {
         .build(&env::var("TOKENSERVER_DATABASE_URL")?).await?;
 
     // Make a simple query to return the given parameter
-    let mut cursor = sqlx::query("SELECT email, client_state from users")
+    let mut rows = sqlx::query("SELECT uid, service, email, generation, client_state, created_at, replaced_at, nodeid, keys_changed_at from users")
+        .map(|row: MySqlRow| {
+            // map the row into a user-defined domain type
+            TokenserverUser {
+                uid: row.get(0),
+                service: row.get(1),
+                email: row.get(2),
+                generation: row.get(3),
+                client_state: row.get(4),
+                created_at: row.get(5),
+                replaced_at: row.get(6),
+                nodeid: row.get(7),
+                keys_changed_at: row.get(8),
+            }
+        })
         .fetch(&pool);
-    while let Some(row) = cursor.next().await? {
-        // map the row into a user-defined domain type
-        let mut i: usize = 0;
-        while i < row.len() {
-            let col: String = row.get(i);
-            println!("col {:?}", col);
-            i += 1;
+        while let Some(row) = rows.try_next().await? {
+            println!("row! {:?}", row);
         }
-    }
-    Ok(())
+            Ok(())
 }
 fn main() -> Result<(), MyError> {
     task::block_on(async_main())
